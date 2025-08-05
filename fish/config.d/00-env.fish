67425ff5c042815033cc3e4cf075bc7fdcb9a936
Glenon Mateus <glenonmateus+github@gmail.com>
2025-08-05 12:34:48 -0300

    feat: add fish configuration

---
+++ fish/config.d/00-env.fish
@@ -0,0 +1,5 @@
+# Envs
+if type -q nvim
+    set -gx EDITOR nvim
+    set -gx MANPAGER "nvim +Man!"
+end
---
+++ fish/config.d/01-alias.fish
@@ -0,0 +1,11 @@
+# Alias
+if type -q curl
+    alias myip 'curl -4 ip.me'
+end
+if type -q aws-vault
+    alias aws-tlac 'aws-vault exec glenon.mateus@proveedor.toyota.com.ar'
+end
+if type -q bat
+    alias cat 'bat -p'
+end
+alias ip 'ip --color=auto'
