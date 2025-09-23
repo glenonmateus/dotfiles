864d52e4423d128180d5f3c11367ce0a92e37f30
Glenon Mateus <glenonmateus+github@gmail.com>
2025-09-23 18:02:03 -0300

    feat: add fish configuration

---
+++ fish/config.fish
@@ -0,0 +1,33 @@
+fish_add_path -a ~/.local/bin
+
+function fish_prompt -d "Write out the prompt"
+    # This shows up as USER@HOST /home/user/ >, with the directory colored
+    # $USER and $hostname are set by fish, so you can just use them
+    # instead of using `whoami` and `hostname`
+    printf '%s@%s %s%s%s > ' $USER $hostname \
+        (set_color $fish_color_cwd) (prompt_pwd) (set_color normal)
+end
+
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+    set fish_greeting
+end
+
+if type -q zoxide
+    zoxide init --cmd cd fish | source
+end
+
+if type -q starship
+    starship init fish | source
+end
+
+for CONFIG in ~/.config/fish/conf.d/*.fish
+    source $CONFIG
+end
+
+# pnpm
+set -gx PNPM_HOME "/home/glenon/.local/share/pnpm"
+if not string match -q -- $PNPM_HOME $PATH
+  set -gx PATH "$PNPM_HOME" $PATH
+end
+# pnpm end
