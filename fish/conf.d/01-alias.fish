637f173307788cf8c720ad576066d426b5b6d8b8
Glenon Mateus <glenonmateus+github@gmail.com>
2026-03-02 18:46:27 -0300

    feat: add kitty ssh configuration

--- fish/conf.d/01-alias.fish
+++ fish/conf.d/01-alias.fish
@@ -5,14 +5,14 @@ if type -q curl
     alias myip 'curl -4 ip.me'
 end
 
-if type -q aws-vault
-
-end
-
 if type -q bat
     alias cat 'bat -p'
 end
 
 if type -q eza
     alias ls 'eza --icons --header --git'
+end
+
+if type -q kitty
+    alias ssh 'kitty +kitten ssh'
 end
